99b2939669338994a6b79a9c03377cf1bb6b3e71
jjessgrace08-ops <jjessgrace08@gmail.com>
2026-07-21 14:15:26 +0700

    Add files via upload

---
+++ 03_insert employee.sql
@@ -0,0 +1,16 @@
+INSERT INTO Employee
+(employee_name,email,position)
+
+VALUES
+
+('Alice Johnson','alice@company.org','Program Officer'),
+
+('Michael Tan','michael@company.org','Finance Officer'),
+
+('Sarah Lee','sarah@company.org','IT Administrator'),
+
+('David Kim','david@company.org','HR Officer'),
+
+('Emma Chen','emma@company.org','Operations Officer');
+
+SELECT * FROM Employee;
